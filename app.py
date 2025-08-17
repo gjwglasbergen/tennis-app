4f2d91886eaa561741f4ceee3243c0086e87e317
Govert Jan Glasbergen <gjwglasbergen@gmail.com>
2025-08-17 15:49:57 +0200

    Implement sockets

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask
+from flask_socketio import SocketIO
+import uuid
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "tennisgoof"
+
+socketio = SocketIO(app)
 
 
 @app.route("/")
 def home():
-    return "Hello world test"
+    return "Hello world test socket"
+
+
+@app.route("/create-match")
+def create_match():
+    return "Hier kun je een match aanmaken"
+
+
+@app.route("/match")
+def get_match():
+    return "Hier komt een match"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, debug=True)
