eff5b309a9ac03892762e47c85d6337fb5061c5a
govertjang <gglasbergen@hmc-heerema.com>
2025-08-18 17:12:59 +0200

    added icon

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from models import db, MatchModel
+from tennis.match import TennisMatch
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "tennisgoof"
