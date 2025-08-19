02e5e7f19e3287b53f0bef1064afca8f1932bcb0
govertjang <gglasbergen@hmc-heerema.com>
2025-08-19 15:26:57 +0200

    database interaction

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from models import db, MatchModel
 from tennis.match import TennisMatch
@@ -20,8 +20,33 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/create-match")
+@app.route("/create-match", methods=["POST"])
 def create_match():
+    if request.method == "POST":
+        player1 = request.form["player1"]
+        player2 = request.form["player2"]
+        num_games_to_win = int(request.form["num_games_to_win"])
+        best_of_num_sets = int(request.form["best_of_num_sets"])
+        whos_serve_player = request.form["whos_serve"]
+        whos_serve = request.form[whos_serve_player]
+        with_AD = request.form["with_AD"]
+
+        matchFormat = {
+            "num_games_to_win": num_games_to_win,
+            "best_of_num_sets": best_of_num_sets,
+            "whos_serve": whos_serve,
+            "with_AD": with_AD,
+        }
+
+        tennis_match = TennisMatch(
+            player1=player1, player2=player2, matchFormat=matchFormat
+        )
+
+        match_model = MatchModel(player1=player1, player2=player2)
+        match_model.set_match(tennis_match)
+        db.session.add(match_model)
+        db.session.commit()
+
     return render_template("creatematch.html")
 
 
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import pickle
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -8,6 +9,12 @@ class MatchModel(db.Model):
     __tablename__ = "matches"
 
     id = db.Column(db.Integer, primary_key=True)
+    player1 = db.Column(db.String(255), nullable=False)
+    player2 = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(
+        db.DateTime, nullable=False, default=datetime.now(timezone.utc)
+    )
+    active = db.Column(db.Boolean, nullable=False, default=False)
     data = db.Column(db.LargeBinary, nullable=False)
 
     def set_match(self, match_object):
